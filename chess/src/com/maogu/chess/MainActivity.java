4342f0f82fe61451a6a804f0d0d9772cb7b16560
liangmaogu <liangmaogu@gmail.com>
2013-07-20 16:45:34 +0800

    Signed-off-by: liangmaogu <liangmaogu@gmail.com>

--- chess/src/com/maogu/chess/MainActivity.java
+++ chess/src/com/maogu/chess/MainActivity.java
@@ -6,7 +6,6 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.view.Display;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
